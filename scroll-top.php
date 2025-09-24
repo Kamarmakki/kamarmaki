ceb2291cae81c1a5e2a0da106d8096919872e1f5
Kamarmakki <kamar.alzman993@gmail.com>
2025-09-24 10:05:37 +0300

    Delete scroll-top.php

--- scroll-top.php
+++
@@ -1,6 +0,0 @@
-<?php
-// k: scroll top button
-?>
-<button id="scroll-top" class="fixed bottom-4 right-4 hidden bg-primary-600 text-white p-2 rounded-full shadow" aria-label="<?php esc_attr_e('Scroll to top', 'kamar'); ?>">
-  <svg class="w-5 h-5"><use href="#icon-up"></use></svg>
-</button>
